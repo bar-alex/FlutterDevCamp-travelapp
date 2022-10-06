8f2b89b26414649d37d3d394770f89f90a030bd5
Eduard Alexandru <eduard.github@gmail.com>
2022-10-06 18:35:54 +0100

    working on it

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterdevcamp_travelapp/models/data.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -42,58 +43,149 @@ class Home extends StatelessWidget {
               style: GoogleFonts.lobster(fontSize: 28),
             ),
           )),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-
-          Image.network(
-            "https://image.freepik.com/free-vector/travel-concept-vector-illustration-with-famous-sights-accessories_95169-98.jpg",
-            color: Colors.green[700],
-            colorBlendMode: BlendMode.colorBurn,
-          ),
-
-          const SizedBox(height: 15),
-
-          Container(
-            height: 50,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: 10,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+      
+            Image.network(
+              "https://image.freepik.com/free-vector/travel-concept-vector-illustration-with-famous-sights-accessories_95169-98.jpg",
+              color: Colors.green[700],
+              colorBlendMode: BlendMode.colorBurn,
+            ),
+      
+            const SizedBox(height: 15),
+      
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: SizedBox(
+                height: 50,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  // scrollDirection: Axis.horizontal,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Container(
+                        // height: 50,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: Colors.red.shade800,
+                        ),
+                        child: const Center(
+                          child: Text( "Attraction",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                              )),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Container(
+                        // height: 50,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: Colors.red.shade800,
+                        ),
+                        child: const Center(
+                          child: Text( "Places",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                              )),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Container(
+                        // height: 50,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: Colors.red.shade800,
+                        ),
+                        child: const Center(
+                          child: Text( "Hotels",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                              )),
+                        ),
+                      ),
+                    ),
+                  
+                  ]
+                )
+              ),
+            ), 
+      
+            const SizedBox(height: 15),
+      
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Text("Popular Destination", 
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                )),
+            ),
+      
+            GridView.builder(
+              primary: false,
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(10),
+              
+              // scrollDirection: Axis.vertical,
+              itemCount: dataList.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, 
+                childAspectRatio: 1,
+                mainAxisSpacing: 20,
+                crossAxisSpacing: 20,
+                ),
               itemBuilder: (BuildContext context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Container(
-                    // height: 50,
-                    width: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      color: Colors.red.shade800,
+                return Stack(
+                  children: [
+                    // the image
+                    Image.network( 
+                      dataList[index].cityImage, 
+                      fit: BoxFit.cover, 
+                      height: 200 
                     ),
-                    child: const Center(
-                      child: Text("Hotels",
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.white,
-                          )),
-                    ),
-                  ),
+
+                    // the text at the bottom (spacer + container with text)
+                    Column(
+                      children: [
+                        const Spacer(flex: 2),
+                        Container(
+                          width: 200,
+                          color: Colors.white.withAlpha(180),
+                          child: Expanded(
+                            child: Center(
+                              child: Text( dataList[index].cityName, 
+                                style: TextStyle(
+                                    color: Colors.red.shade800, 
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w600,
+                                  ) 
+                                ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ]
                 );
               },
             ),
-          ),
-
-          const SizedBox(height: 15),
-          
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Text("Popular destinations", 
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
-              )),
-          ),
-
-        ],
+      
+          ],
+        ),
       ),
     );
   }
